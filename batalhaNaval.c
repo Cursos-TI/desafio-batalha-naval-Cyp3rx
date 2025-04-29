3824dea0e65146622b0712d8cc042e77628a74d9
Cyp3rx <138186020+Cyp3rx@users.noreply.github.com>
2025-04-29 23:37:38 +0000

    Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,36 +6,43 @@
 int main() {
     char tabuleiro[LINHAS][COLUNAS];
 
-    // Inicializar o tabuleiro 
+    // Inicializar o tabuleiro
     for (int i = 0; i < LINHAS; i++) {
         for (int j = 0; j < COLUNAS; j++) {
             tabuleiro[i][j] = '0';
         }
     }
 
-    // Posicionar navios com '3'
-
-    // Navio na horizontal 
-    for (int j = 0; j < 4; j++) {
+    // Navio horizontal 
+    for (int j = 0; j < 3; j++) {
         tabuleiro[1][j] = '3';
     }
 
-    // Navio na vertical 
+    // Navio vertical 
     for (int i = 4; i < 7; i++) {
         tabuleiro[i][5] = '3';
     }
 
+    // Navio diagonal principal ↘ 
+    for (int i = 6; i < 9; i++) {
+        tabuleiro[i][i] = '3';
+    }
 
-    // Cabeçalho 
+    // Novo navio diagonal secundária ↙ 
+    tabuleiro[0][9] = '3';
+    tabuleiro[1][8] = '3';
+    tabuleiro[2][7] = '3';
+
+    // Imprimir o cabeçalho
     printf("   ");
     for (int j = 0; j < COLUNAS; j++) {
         printf(" %c ", 'A' + j);
     }
     printf("\n");
 
-    // Linhas 
+    // Imprimir o tabuleiro
     for (int i = 0; i < LINHAS; i++) {
-        printf("%2d ", i + 1); // número da linha
+        printf("%2d ", i + 1);
         for (int j = 0; j < COLUNAS; j++) {
             printf(" %c ", tabuleiro[i][j]);
         }
